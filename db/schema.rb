df5fcca87e1bb846b6edb0f3bb380e384a88f674
Harsh <agrawal.harsh14@gmail.com>
2019-01-02 13:46:33 +0530

    Comments added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_01_124706) do
+ActiveRecord::Schema.define(version: 2019_01_02_075243) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
